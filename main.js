538988c2261daeb90eeb249ba4c184d8781aca2a
coscorrodrift <juan.dyamamoto@gmail.com>
2022-05-25 03:03:24 +0200

    committed sth just so it doesnt get in the way

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const screen = document.getElementById('screen')
 
-const calc = new Calculator(screen)
+
+const numbers = ['zero', 'one','two','three','four','five','six','seven','eight','nine']
+
+const numberElements = numbers.map( number => document.getElementById(number))
 
 
+document.getElementById('one').addEventListener('click', one)
 
-function Calculator(sc) {
-    this.screen = sc
-    
-    this.getMethods = function(){
-        document.querySelectorAll('span')
+function one (){
+        
+    if (screen.value.length < 11) {
+        screen.value += `${numbers.indexOf(numberLiteral)}`
     }
-} 
+    
+}
+
+console.log(numberElements)
+
+
+
